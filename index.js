31d405d24ad9576b276bb72ea13abbb03b89dd9d
kipkorir73 <123023142+kipkorir73@users.noreply.github.com>
2023-07-02 17:33:31 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('http://localhost:3000/films/1')
+    .then(response => response.json())
+    .then(data => {
+      updateMovieDetails(data);
+    })
+    .catch(error => console.log(error));
+
+  fetch('http://localhost:3000/films')
+    .then(response => response.json())
+    .then(data => {
+      const filmsList = document.getElementById('films');
+      data.forEach(film => {
+        const listItem = document.createElement('li');
+        listItem.textContent = film.title;
+        listItem.classList.add('film', 'item');
+        filmsList.appendChild(listItem);
+
+        listItem.addEventListener('click', function() {
+          document.getElementById('main-title').innerHTML = film.title;
+          fetch(`http://localhost:3000/films/${film.id}`)
+            .then(response => response.json())
+            .then(data => {
+              updateMovieDetails(data);
+            })
+            .catch(error => console.log(error));
+        });
+      });
+    });
+
+  function updateMovieDetails(movie) {
+    const movieDetails = document.getElementById('movie-details');
+    const moviePoster = movieDetails.querySelector('#movie-poster');
+    const movieTitle = movieDetails.querySelector('#movie-title');
+    const movieRuntime = movieDetails.querySelector('#movie-runtime');
+    const movieShowtime = movieDetails.querySelector('#movie-showtime');
+    const movieAvailableTickets = movieDetails.querySelector('#movie-available-tickets');
+    const buyTicketButton = movieDetails.querySelector('#buy-ticket');
+
+    moviePoster.src = movie.poster;
+    movieTitle.textContent = movie.title;
+    movieRuntime.textContent = `Runtime: ${movie.runtime} minutes`;
+    movieShowtime.textContent = `Showtime: ${movie.showtime}`;
+    const availableTickets = movie.capacity - movie.tickets_sold;
+    movieAvailableTickets.textContent = `Available Tickets: ${availableTickets}`;
+
+    if (availableTickets === 0) {
+      buyTicketButton.disabled = true;
+      buyTicketButton.textContent = 'Sold Out';
+    } else {
+      buyTicketButton.disabled = false;
+      buyTicketButton.textContent = 'Buy Ticket';
+    }
+
+    buyTicketButton.addEventListener('click', function() {
+      if (availableTickets > 0) {
+        availableTickets--;
+        movieAvailableTickets.textContent = `Available Tickets: ${availableTickets}`;
+      }
+    });
+  }
+});
